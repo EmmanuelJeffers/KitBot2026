80bc1261d94666be578e5916288ac65dffdb875b
EmmanuelJeffers <ejeffers22@gmail.com>
2026-02-20 22:50:57 -0500

    maybe fixed gradle problem?

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "KitBot2026";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "d1ef7f048adb53f02118eb5e3d406975b831b10a";
+  public static final String GIT_DATE = "2026-02-20 22:12:43 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-20 22:45:33 EST";
+  public static final long BUILD_UNIX_TIME = 1771645533868L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
